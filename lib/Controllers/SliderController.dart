e27ccaf0a75d956594a3b40b2c47ffa8236777ac
dani <danyalshahangi@gmail.com>
2024-06-21 13:54:28 +0330

    last commit

--- lib/Controllers/SliderController.dart
+++ lib/Controllers/SliderController.dart
@@ -1,5 +1,18 @@
 import 'package:get/get.dart';
+import 'package:slider_test/Model/SliderModel.dart';
+import 'package:slider_test/service/sliderapi.dart';
 
-class Sliders extends GetxController {
-  RxList SliderList = <int>[].obs;
+class SliderGETX extends GetxController {
+  RxList SliderList = <SliderModel>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    fetchslider();
+  }
+
+  Future<void> fetchslider() async {
+    SliderList.value = await SliderApi.getList();
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:slider_test/Controllers/SliderController.dart';
+import 'package:slider_test/Model/SliderModel.dart';
 
 void main(List<String> args) {
   runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  MainApp({super.key});
+
+  final SliderGETX Slider = Get.put(SliderGETX());
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Column(
-          children: [
-            Container(
-              child: Text('${0}'),
-            )
-          ],
+          children: List.generate(Slider.SliderList.length, (index) {
+            SliderModel data = Slider.SliderList[index];
+            return Column(
+              children: [
+                Text(data.id),
+                Text(data.categoryID),
+                Text(data.linkPhoto),
+                Text(data.nameManufacturer)
+              ],
+            );
+          }),
         ),
       ),
     );
